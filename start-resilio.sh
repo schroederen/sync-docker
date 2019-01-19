a2f3b16c29d4d9b6763aeb4210bc6e0dfe8f0dc0
Enrico Schroeder <enrico.schroeder@outlook.com>
2019-01-19 18:15:15 +0100

    Set container to restart always

--- start-resilio.sh
+++ start-resilio.sh
@@ -9,5 +9,5 @@ docker run -d --name Sync \
            -p 0.0.0.0:$WEBUI_PORT:8888 \
            -p 55555 \
            -v $DATA_FOLDER:/mnt/sync \
-           --restart on-failure \
+           --restart always \
            rslsync
